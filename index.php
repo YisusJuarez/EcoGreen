d241496afc87327b14d4d5d558357ad00f97a19e
YisusJuarez <yisusjuarezo@hotmail.com>
2020-02-13 19:01:49 -0600

    version final

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
     <div class="collapse navbar-collapse " id="navbarTogglerDemo02">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
         <li class="nav-item ">
-          <a class="nav-link" href="#inicio">Inicio </a>
+          <a class="nav-link" href="./index.php">Inicio </a>
         </li>
         <li class="nav-item d-md-block d-none">
           <a class="nav-link" data-scroll href="#contacto">Contacto</a>
